7858ace4ec17e9ac0f4073a5e17844a684cbee7c
SoonYoung-Kwon <soonyoung3a@gmail.com>
2021-08-06 14:05:48 +0900

    Add : fuction setMode is change show components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.scss';
+
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import DronInfo from "./components/DronInfo"
 import MapData from "./components/MapData"
 
+import { useState } from "react";
+
 function App() {
+  
+  const [mode, setMode] = useState("B");
+
+  const setA = () => {
+    console.log('A');
+    setMode("A");
+  }
+
+  const setB = () => {
+    console.log('B');
+    setMode("B");
+  }
+
+  const setC = () => {
+    console.log('C');
+    setMode("C");
+  }
+
   return (
     <div>
         <Header/>
         <div className="row">
-          <Navbar/>
-          <MapData/>
-        </div>
-        <Header/>
-        <div className="row">
-          <Navbar/>
-          <DronInfo/>
+          <Navbar mode={mode} modeA={setA} modeB={setB} modeC={setC}/>
+          {mode === "A" &&
+            <MapData/>
+          }
+          {mode === "B" &&
+            <DronInfo/>
+          }
         </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar( { mode, modeA, modeB, modeC } ) {
+
     return (
         <div className="row">
-            <div className="navbar-icons"></div>
-            <div className="navbar-menu"></div>
+            <div className="navbar-iconbar"></div>
+            <div className="navbar-menubar">
+                <div className={"navbar-menu " + ( mode === "A" ? 'active' : '' )} onClick={modeA}>
+                    지도 및 데이터
+                </div>
+                <div className={"navbar-menu " + ( mode === "B" ? 'active' : '' )} onClick={modeB}>
+                    드론 정보
+                </div>
+                <div className={"navbar-menu " + ( mode === "C" ? 'active' : '' )} onClick={modeC}>
+                    사용자 정보
+                </div>
+            </div>
         </div>
     )
 }
